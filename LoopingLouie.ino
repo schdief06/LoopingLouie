b1d4457f4e9ec30b34496389dab0319fb6becd3b
schdief06 <stefan.mueller93@web.de>
2018-06-20 17:23:08 +0200

    Initial commit

---
+++ LoopingLouie.ino
@@ -0,0 +1,101 @@
+#include <AFMotor.h>
+#define MAXSPEED 100
+#define MINSPEED 40
+
+#define SPEEDCHANGE 3000
+
+AF_DCMotor motorL(1);
+
+//unsigned long time = 0;
+//unsigned long timeMode = 0;
+unsigned long targetTimeMode = 0;
+unsigned long nextSpeedChange = 0;
+unsigned int targetSpeed = MINSPEED;
+
+unsigned int rndfor = 0;
+
+typedef struct{
+    int minspeed;
+    int maxspeed;
+    int mintime;
+    int maxtime;
+    bool forward;
+    bool randomspeed;
+} mode;
+
+//minspeed, maxspeed, mintime, maxtime, forward/backward (1/-1), randomspeed 1/0
+
+const mode modeForward = {MINSPEED, MAXSPEED, 2000, 30000, true, true};
+const mode modeBackward = {MINSPEED, MAXSPEED, 500, 1000, false, true};
+const mode modeDrop = {0, 0, 1000, 1000, true, false};
+const mode modeEscalate = {MAXSPEED, MAXSPEED, 5000, 5000, true, false};
+
+mode modes[] = {modeForward, modeBackward, modeDrop, modeEscalate};
+mode *currentMode = NULL;
+
+
+//void setSpeed(int newspeed) {
+//  if (newspeed > MAXSPEED + 5){ // guard for algorithm fail
+//    speed = 0;
+//  } else {
+//    speed = newspeed;
+//  }
+//}
+
+void nextMode() {
+  int index = random(0, sizeof(modes) / sizeof(mode));
+  currentMode = &modes[index];
+  //timeMode = millis();
+  targetTimeMode = millis() + random(currentMode->mintime, currentMode->maxtime);
+  targetSpeed = random(currentMode->minspeed, currentMode->maxspeed);
+
+  Serial.print("Mode changed: ");
+  Serial.print(index);
+  Serial.print("\n");
+}
+
+uint8_t speed = 0;
+uint8_t generateSpeed() {
+  if (currentMode->randomspeed) {
+      if(millis() > nextSpeedChange){
+        targetSpeed = random(currentMode->minspeed, currentMode->maxspeed);        
+      }
+  } else {
+    nextSpeedChange = millis() + SPEEDCHANGE;
+  }
+
+  if(speed < targetSpeed) {
+     speed = (speed+2);
+  } else {
+     speed = (speed-2);
+  }
+
+  speed = min(speed, MAXSPEED);
+  
+  return speed;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  motorL.run(RELEASE);
+  targetTimeMode = millis();
+  nextMode();
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(millis() > targetTimeMode){
+    nextMode();
+  }
+  motorL.setSpeed(generateSpeed());
+  
+  if(currentMode->forward) {
+    motorL.run(FORWARD);
+  } else {
+    motorL.run(BACKWARD);
+  }
+  
+  delay(100);
+}
+
